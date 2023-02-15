8336324ed86d395db9287a128d90b7f35d8eaf33
craig-db <craig-db>
2023-02-15 16:04:16 +0000

    Secrets file w/ placeholders

---
+++ Secrets.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+SR_URL="https://ABC.aws.confluent.cloud"
+SR_API_KEY="ABC"
+SR_API_SECRET="ABC"
+
+KAFKA_KEY="ABC"
+KAFKA_SECRET="ABC"
+KAFKA_SERVER="ABC.aws.confluent.cloud:9092"
+KAFKA_TOPIC = "app-events"
+WRAPPER_TOPIC = "wrapper"
+
+CHECKPOINT_LOCATION = "dbfs:/tmp/ABC/demux_cp_location"
