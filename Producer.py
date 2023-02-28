11a0d0c1daad4f839af82e228e54b5f00b7f216e
craig-db <craig-db>
2023-02-28 00:48:11 +0000

    allow from version 1

--- Producer.py
+++ Producer.py
@@ -200,7 +200,7 @@ latest_version = 0
 # COMMAND ----------
 
 # DBTITLE 1,Send simulated payload messages to Kafka
-for version in range(1, NUM_VERSIONS):
+for version in range(1, NUM_VERSIONS + 1):
   # Starting at 1 because Confluent is free for 10 partitions; one partition is needed for the wrapper topic/schema
   for target in range(1, NUM_TARGET_TABLES): 
     latest_version = max(version, latest_version)
--- Secrets.py
+++ Secrets.py
@@ -1,12 +1,12 @@
 # Databricks notebook source
-SR_URL="https://ABC.aws.confluent.cloud"
-SR_API_KEY="ABC"
-SR_API_SECRET="ABC"
+SR_URL="https://psrc-68gz8.us-east-2.aws.confluent.cloud"
+SR_API_KEY="HGXZS5EZYSDN5VVK"
+SR_API_SECRET="YXOdcTzlFqNE68Nl5wt6RnmzFSumvzmvCdLSVbVwQ5dSzdZqul9X3/7uVrmspbyL"
 
-KAFKA_KEY="ABC"
-KAFKA_SECRET="ABC"
-KAFKA_SERVER="ABC.aws.confluent.cloud:9092"
+KAFKA_KEY="BKN2DJIHJUCVONGE"
+KAFKA_SECRET="qCrbJa2kdD+sF3SKtOi1rLy8moybg/vnzX7zd4br9SYe7O2S3MTx5WuPXExGpQnY"
+KAFKA_SERVER="pkc-pgq85.us-west-2.aws.confluent.cloud:9092"
 KAFKA_TOPIC = "app-events"
 WRAPPER_TOPIC = "wrapper"
 
-CHECKPOINT_LOCATION = "dbfs:/tmp/ABC/demux_cp_location"
+CHECKPOINT_LOCATION = "dbfs:/tmp/craig_lukasik/demux_cp_location"
